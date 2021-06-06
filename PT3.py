9876fddf6b2198784bef1553f58678415b639e39
illusion98 <83572664+illusion98@users.noreply.github.com>
2021-06-06 21:34:23 +0900

    Update PT3.py

--- PT3.py
+++ PT3.py
@@ -7,19 +7,19 @@ def main():
 	gpio.pinMode(0,gpio.OUT) # led , digitalWrite
 	gpio.pinMode(1,gpio.IN) # button, digitalRead
 	gpio.pinMode(2,gpio.IN) # tempsensor
-	gpio.pinMode(3,gpio.OUT) # heating
+	#gpio.pinMode(3,gpio.OUT) # heating
 	
 	# pin 상태, output -> HIGH(3.3v)/ LOW(0v)
 	while 1:
 		tempsensor=gpio.digitalRead(2)
 		temperature=(tempsensor*200)/1023-100
 		
-		print('tempsensor:{0:3.2f}. temperature'.format(tempsensor,temperature))
+		print('tempsensor:{0:3.2f}. temperature:{}'.format(tempsensor,temperature))
 		
 		if (temperature>21):
-			digitalWrite(0,gpio.HIGH)
+			gpio.digitalWrite(0,gpio.HIGH)
 		else:
-			digitalWrite(0,gpio.LOW)
+			gpio.digitalWrite(0,gpio.LOW)
 		
 		button=gpio.digitalRead(1)
 		if (button==gpio.HIGH):
