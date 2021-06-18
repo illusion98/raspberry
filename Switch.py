3fd0eab3dec731a1792162cc7c399e13939a7b76
illusion98 <cho98516@naver.com>
2021-06-18 10:46:52 +0100

    switch + led

--- Switch.py
+++ Switch.py
@@ -1,24 +1,21 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-import time
+import RPi.GPIO as GPIO
+from time import sleep
 
-LED = 10
-Switch= 12
+LED= 12
+Switch= 8
 
-
-GPIO.setwarnings(False) # Ignore warning for now
+#GPIO.setmode(GPIO.BCM) # set up BCM GPIO numbering
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-
-# Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(Switch, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(Switch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(LED, GPIO.OUT) 
 
 while True:
     
-    if GPIO.input(Switch) == GPIO.LOW:
+    if GPIO.input(Switch) == GPIO.HIGH:
         print("Button was pushed!")
         GPIO.output(LED, GPIO.HIGH)
-        time.sleep(1)
+        sleep(1)
     else:
         print("Button was not pushed!")
         GPIO.output(LED, GPIO.LOW)
-        time.sleep(1)
+        sleep(1)
