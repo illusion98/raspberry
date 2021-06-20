2452cc710b32065a155bc1b822d88bb80f1a6d77
illusion98 <cho98516@naver.com>
2021-06-20 21:35:32 +0100

    PWM test

--- led+sw/led_PWM.py
+++ led+sw/led_PWM.py
@@ -8,13 +8,14 @@ LED=12
 def main():
     GPIO.setup(LED, GPIO.OUT)
     
-    PWM_LED=GPIO.PWM(LED, 50) #(channel,frequency)
-    PWM_LED.start(0) #(dutycycle)
+    PWM_LED=GPIO.PWM(LED, 50)
+    PWM_LED.start(0)
     
     while 1:
-        Duty=input()
-        duty=int(Duty)
-        PWM_LED.ChangeDutyCycle(duty)
+        for duty in range(100):
+            PWM_LED.ChangeDutyCycle(duty)
+            time.sleep(0.5)
         
 if __name__=='__main__':
     main()
+    
